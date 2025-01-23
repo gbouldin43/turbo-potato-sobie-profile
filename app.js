02fd3186d09c38aef07c68bd98134ccb2856647c
Bouldin <gbouldin@una.edu>
2025-01-23 16:18:07 -0600

    Code for 1/23/2025

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000;
+const bodyParser = require('body-parser')
 
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function (req, res) {
   res.sendFile('index.html')
 })
 
+app.post('/saveMyName', (req,res)=>{
+  console.log('did we hit the endpoint?');
+
+  console.log(req.body);
+
+  res.redirect('/ejs');
+})
+
 app.get('/ejs', function (req, res) {
-  res.render('words');
+  res.render('words', {pageTitle: 'my cool ejs page'});
 })
 
 app.get('/nodemon', function (req, res) {
@@ -22,4 +32,4 @@ app.get('/helloRender', function (req, res) {
   res.send('Hello Express from Real World<br><a href="/">back to home</a>')
 })
 
-app.listen(port, ()=> console.log(`server is running on ... ${port}`));
+app.listen(port, ()=> console.log(`server is running on ... localhost:${port}`));
